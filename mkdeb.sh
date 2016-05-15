020fe62e43a551ece001fb752a7bfd54782ad178
hfoffani <hfoffani@gmail.com>
2016-05-15 22:19:08 +0200

    arreglo cosas del lint

--- mkdeb.sh
+++ mkdeb.sh
@@ -3,12 +3,23 @@ mkdir -p deb/usr/local/bin
 mkdir -p deb/usr/local/share/freeling
 
 cp /usr/local/lib/lib* deb/usr/local/lib/
-cp -r /usr/local/share/freeling/* deb/usr/local/share/freeling/
 
+cd deb/usr/local/lib
+rm libfreeling.so
+rm libfoma.so
+rm libtreeler.so
+chmod 644 *.so
+ln -s libfreeling-4.0-beta2.so libfreeling.so
+ln -s libfoma-0.9.18.so libfoma.so
+ln -s libtreeler-0.4.so libtreeler.so
+cd ../../../..
+
+cp -r /usr/local/share/freeling/* deb/usr/local/share/freeling/
 cp /usr/local/bin/* deb/usr/local/bin/
+
 mkdir deb/DEBIAN
 find deb -type d | xargs chmod 755
 cp control deb/DEBIAN/
-fakeroot dpkg-deb --build deb
+dpkg-deb --build deb
 
 mv deb.deb freeling4.deb
