38d4869a945119150fef96c1e3d20c19be4a0712
hfoffani <hfoffani@gmail.com>
2016-05-16 11:17:16 +0200

    stripping. pero no funciona.

--- mkdeb.sh
+++ mkdeb.sh
@@ -3,11 +3,24 @@ mkdir -p deb/usr/local/bin
 mkdir -p deb/usr/local/share/freeling
 
 cp /usr/local/lib/lib* deb/usr/local/lib/
+cp /usr/local/bin/* deb/usr/local/bin/
+
+rm deb/usr/local/lib/libfreeling.so
+rm deb/usr/local/lib/libfoma.so
+rm deb/usr/local/lib/libtreeler.so
+
+
+objcopy -S deb/usr/local/bin/analyzer
+objcopy -S deb/usr/local/bin/analyzer_client
+objcopy -S deb/usr/local/bin/dicc2phon
+objcopy -S deb/usr/local/bin/threaded_analyzer
+objcopy -S deb/usr/local/bin/treeler
+objcopy -S deb/usr/local/lib/libfoma-0.9.18.so
+objcopy -S deb/usr/local/lib/libfreeling-4.0-beta2.so
+objcopy -S deb/usr/local/lib/libtreeler-0.4.so
+
 
 cd deb/usr/local/lib
-rm libfreeling.so
-rm libfoma.so
-rm libtreeler.so
 chmod 644 *.so
 ln -s libfreeling-4.0-beta2.so libfreeling.so
 ln -s libfoma-0.9.18.so libfoma.so
@@ -16,8 +29,6 @@ cd ../../../..
 
 cp -r /usr/local/share/freeling/* deb/usr/local/share/freeling/
 find deb/usr/local/share/freeling/ -type f | xargs chmod 644
-
-cp /usr/local/bin/* deb/usr/local/bin/
 
 mkdir deb/DEBIAN
 find deb -type d | xargs chmod 755
